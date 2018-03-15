075376aacc5a76ca0571b71ea7d7227718cb0987
Welson <welsonlimawlsn@gmail.com>
2018-03-15 12:03:32 -0300

    Criação do projeto é configuração do banco de dados

---
+++ src/main/java/br/com/welson/examgenerator/ExamGeneratorApplication.java
@@ -0,0 +1,12 @@
+package br.com.welson.examgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGeneratorApplication.class, args);
+	}
+}
