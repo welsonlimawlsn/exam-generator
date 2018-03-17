b2e67f4b4d7401d283f82adba655029a62084851
Welson <welsonlimawlsn@gmail.com>
2018-03-17 17:22:58 -0300

    Documentação com Swagger

--- src/main/java/br/com/welson/examgenerator/endpoint/v1/ProfessorEndpoint.java
+++ src/main/java/br/com/welson/examgenerator/endpoint/v1/ProfessorEndpoint.java
@@ -1,17 +1,32 @@
 package br.com.welson.examgenerator.endpoint.v1;
 
+import br.com.welson.examgenerator.persistence.model.Professor;
+import br.com.welson.examgenerator.persistence.repository.ProfessorRepository;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("v1/professor")
 public class ProfessorEndpoint {
 
-    @GetMapping
-    public ResponseEntity<?> hi() {
-        return new ResponseEntity<>("Hi", HttpStatus.OK);
+    private final ProfessorRepository professorRepository;
+
+    @Autowired
+    public ProfessorEndpoint(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    @GetMapping(path = "{id}")
+    @ApiOperation(value = "Find professor by his ID", notes = "We have to make this method better", response = Professor.class)
+    public ResponseEntity<?> getProfessorById(@PathVariable long id) {
+        return new ResponseEntity<>(professorRepository.findById(id), HttpStatus.OK);
     }
 }
---
+++ src/main/java/br/com/welson/examgenerator/persistence/repository/ProfessorRepository.java
@@ -0,0 +1,11 @@
+package br.com.welson.examgenerator.persistence.repository;
+
+import br.com.welson.examgenerator.persistence.model.Professor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface ProfessorRepository extends PagingAndSortingRepository<Professor, Long> {
+
+    Optional<Professor> findByEmail(String email);
+}
